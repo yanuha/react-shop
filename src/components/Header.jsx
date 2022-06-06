269538eec372538e0f24d2e76e9f7eebee04e8d3
Evgeniy Yanushko <yanuha2010@mail.ru>
2022-06-06 15:16:55 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,13 @@ function Header() {
         </a>
         <ul id='nav-mobile' className='right hide-on-med-and-down'>
           <li>
-            <a href='!#'>Repo</a>
+            <a
+              href='https://yanuha.github.io/react-shop/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Repo
+            </a>
           </li>
         </ul>
       </div>
